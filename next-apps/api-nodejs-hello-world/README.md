8712ad36573eb7601badea462347564bde02078b
Dario Piotrowicz <dario@cloudflare.com>
2024-08-13 14:08:44 +0100

    add api-nodejs-hello-world Next.js app

---
+++ next-apps/api-nodejs-hello-world/README.md
@@ -0,0 +1,1 @@
+A simple app that only has a single `api/hello` API route (using the `node.js` runtime).
---
+++ next-apps/api-nodejs-hello-world/app/api/hello/route.js
@@ -0,0 +1,8 @@
+import { headers } from 'next/headers';
+
+export async function GET() {
+    // Note: we use headers just so that the route is not built as a static one
+    const headersList = headers()
+    const sayHi = !!headersList.get('should-say-hi');
+    return new Response(sayHi ? 'Hi World!' : 'Hello World!');
+}
