6aa1cd22ce30f395cece4fcc167bec5bd0601d9d
Dario Piotrowicz <dario@cloudflare.com>
2024-08-21 18:47:45 +0100

    fix various Next.js specific `require`s

--- nextjs-worker-builder/src/build/build-worker/index.ts
+++ nextjs-worker-builder/src/build/build-worker/index.ts
@@ -1,6 +1,6 @@
 import { NextjsAppPaths } from "../../nextjsPaths";
 import { build, Plugin } from "esbuild";
-import { readFileSync, writeFileSync } from "node:fs";
+import { existsSync, readFileSync, writeFileSync } from "node:fs";
 import { cp, readFile, writeFile } from "node:fs/promises";
 
 import { globSync } from "glob";
@@ -206,6 +206,56 @@ async function updateWorkerBundledCode(
         console.log("url.pathToFileURL", path);
         return new URL("file://" + path);
       }
+    `
+  );
+
+  // The following avoid various Next.js specific files `require`d at runtime since we can just read
+  // and inline their content during build time
+  const pagesManifestFile = `${nextjsAppPaths.standaloneAppServerDir}/pages-manifest.json`;
+  const appPathsManifestFile = `${nextjsAppPaths.standaloneAppServerDir}/app-paths-manifest.json`;
+
+  const pagesManifestFiles = existsSync(pagesManifestFile)
+    ? Object.values(JSON.parse(readFileSync(pagesManifestFile, "utf-8"))).map(
+        (file) => ".next/server/" + file
+      )
+    : [];
+  const appPathsManifestFiles = existsSync(appPathsManifestFile)
+    ? Object.values(
+        JSON.parse(readFileSync(appPathsManifestFile, "utf-8"))
+      ).map((file) => ".next/server/" + file)
+    : [];
+  const allManifestFiles = pagesManifestFiles.concat(appPathsManifestFiles);
+
+  const htmlPages = allManifestFiles.filter((file) => file.endsWith(".html"));
+  const pageModules = allManifestFiles.filter((file) => file.endsWith(".js"));
+
+  updatedWorkerContents = updatedWorkerContents.replace(
+    /const pagePath = getPagePath\(.+?\);/,
+    `$&
+    ${htmlPages
+      .map(
+        (htmlPage) => `
+          if (pagePath.endsWith("${htmlPage}")) {
+            return ${JSON.stringify(
+              readFileSync(
+                `${nextjsAppPaths.standaloneAppDir}/${htmlPage}`,
+                "utf-8"
+              )
+            )};
+          }
+        `
+      )
+      .join("\n")}
+    ${pageModules
+      .map(
+        (module) => `
+          if (pagePath.endsWith("${module}")) {
+            return require("${nextjsAppPaths.standaloneAppDir}/${module}");
+          }
+        `
+      )
+      .join("\n")}
+    throw new Error("Unknown pagePath: " + pagePath);
     `
   );
 
