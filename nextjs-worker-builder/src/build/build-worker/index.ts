c9e4796bbe0b46e46055ecc22e359b39b9c25225
Dario Piotrowicz <dario@cloudflare.com>
2024-08-23 19:41:55 +0100

    fetch `findDir` utility

--- nextjs-worker-builder/src/build/build-worker/index.ts
+++ nextjs-worker-builder/src/build/build-worker/index.ts
@@ -274,6 +274,25 @@ async function updateWorkerBundledCode(
     `
 	);
 
+	// Here we patch `findDir` so that the next server can detect whether the `app` or `pages` directory exists
+	// (source: https://github.com/vercel/next.js/blob/ba995993/packages/next/src/lib/find-pages-dir.ts#L4-L13)
+	// (usage source: https://github.com/vercel/next.js/blob/ba995993/packages/next/src/server/next-server.ts#L450-L451)
+	// Note: `findDir` uses `fs.existsSync` under the hood, so patching that should be enough to make this work
+	updatedWorkerContents = updatedWorkerContents.replace(
+		"function findDir(dir, name) {",
+		`function findDir(dir, name) {
+			if (dir.endsWith(".next/server")) {
+			if (name === "app") return ${existsSync(
+				`${nextjsAppPaths.standaloneAppServerDir}/app`
+			)};
+			if (name === "pages") return ${existsSync(
+				`${nextjsAppPaths.standaloneAppServerDir}/pages`
+			)};
+		}
+		throw new Error("Unknown findDir call: " + dir + " " + name);
+		`
+	);
+
 	await writeFile(workerOutputFile, updatedWorkerContents);
 }
 
