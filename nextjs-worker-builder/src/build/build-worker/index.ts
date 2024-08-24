ed92e470bd4308a303ba6c4f98f846540b1aef2a
Dario Piotrowicz <dario@cloudflare.com>
2024-08-24 14:31:24 +0100

    fix evalManifest issue

--- nextjs-worker-builder/src/build/build-worker/index.ts
+++ nextjs-worker-builder/src/build/build-worker/index.ts
@@ -293,6 +293,42 @@ async function updateWorkerBundledCode(
 		`
 	);
 
+	// `evalManifest` relies on readFileSync so we need to patch the function so that it instead returns the content of the manifest files
+	// which are known at build time
+	// (source: https://github.com/vercel/next.js/blob/b1e32c5d1f/packages/next/src/server/load-manifest.ts#L72)
+	// Note: we could/should probably just patch readFileSync here or something, but here the issue is that after the readFileSync call
+	// there is a vm `runInNewContext` call which we also don't support (source: https://github.com/vercel/next.js/blob/b1e32c5d1f/packages/next/src/server/load-manifest.ts#L88)
+	const manifestJss = globSync(
+		`${nextjsAppPaths.standaloneAppDotNextDir}/**/*_client-reference-manifest.js`
+	).map((file) => file.replace(`${nextjsAppPaths.standaloneAppDir}/`, ""));
+	updatedWorkerContents = updatedWorkerContents.replace(
+		/function evalManifest\((.+?), .+?\) {/,
+		`$&
+		${manifestJss
+			.map(
+				(manifestJs) => `
+			  if ($1.endsWith("${manifestJs}")) {
+				require("${nextjsAppPaths.standaloneAppDir}/${manifestJs}");
+				return {
+				  __RSC_MANIFEST: {
+					"${manifestJs
+						.replace(".next/server/app", "")
+						.replace(
+							"_client-reference-manifest.js",
+							""
+						)}": globalThis.__RSC_MANIFEST["${manifestJs
+						.replace(".next/server/app", "")
+						.replace("_client-reference-manifest.js", "")}"],
+				  },
+				};
+			  }
+			`
+			)
+			.join("\n")}
+		throw new Error("Unknown evalManifest: " + $1);
+		`
+	);
+
 	await writeFile(workerOutputFile, updatedWorkerContents);
 }
 
