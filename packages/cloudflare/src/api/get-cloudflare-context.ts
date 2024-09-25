a83fba68e9257f42f03d666a04a007f6debc76d1
Dario Piotrowicz <dario@cloudflare.com>
2024-09-25 10:35:02 +0100

    add ts-doc comment to getCloudflareContext function

--- packages/cloudflare/src/api/get-cloudflare-context.ts
+++ packages/cloudflare/src/api/get-cloudflare-context.ts
@@ -16,6 +16,19 @@ export type CloudflareContext<
 
 const cloudflareContextSymbol = Symbol.for("__cloudflare-context__");
 
+/**
+ * Utility to get the current Cloudflare context.
+ *
+ * Such context contains three values:
+ *  - `env`: object containing the worker's bindings
+ *          (https://developers.cloudflare.com/workers/runtime-apis/bindings/)
+ *  - `ctx`: the current execution context object
+ *          (https://developers.cloudflare.com/workers/runtime-apis/context)
+ *  - `cf`: object containing information regarding the current request
+ *          (https://developers.cloudflare.com/workers/runtime-apis/request/#the-cf-property-requestinitcfproperties)
+ *
+ * @returns the cloudflare context
+ */
 export async function getCloudflareContext<
   CfProperties extends Record<string, unknown> = IncomingRequestCfProperties,
   Context = ExecutionContext,
