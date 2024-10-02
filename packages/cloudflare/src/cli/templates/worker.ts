0d98fbc0d91e99a8345c6ab167a6e455e7a00a4f
Dario Piotrowicz <dario@cloudflare.com>
2024-10-02 18:39:51 +0100

    add missing non-null assertion

--- packages/cloudflare/src/cli/templates/worker.ts
+++ packages/cloudflare/src/cli/templates/worker.ts
@@ -64,7 +64,7 @@ export default {
 
       const { req, res, webResponse } = getWrappedStreams(request, ctx);
 
-      ctx.waitUntil(requestHandler(new NodeNextRequest(req), new NodeNextResponse(res)));
+      ctx.waitUntil(requestHandler!(new NodeNextRequest(req), new NodeNextResponse(res)));
 
       return await webResponse();
     });
