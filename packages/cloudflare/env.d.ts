3c84076d59c6a4e263a203fff09d5c6b75507fba
James <james@eli.cx>
2024-10-06 13:12:16 +0100

    comments

--- packages/cloudflare/env.d.ts
+++ packages/cloudflare/env.d.ts
@@ -4,7 +4,7 @@ declare global {
       ASSETS: Fetcher;
       __NEXT_PRIVATE_STANDALONE_CONFIG?: string;
       SKIP_NEXT_APP_BUILD?: string;
-      [key: string]: string | Fetcher;
+      [key: string]: string;
     }
   }
 }
