a52bdaddcad52d770d10c688f91ba1aaaaea9e93
Peter Bacon Darwin <pbacondarwin@cloudflare.com>
2024-10-07 15:01:31 +0100

    fix typo

--- .github/utils/custom-changeset-version.mjs
+++ .github/utils/custom-changeset-version.mjs
@@ -2,7 +2,7 @@ import { execSync } from "node:child_process";
 
 // This script is used by the `changesets.yml` workflow to update the version of the packages being released.
 // The standard step is only to run `changeset version` but this does not update the package-lock.json file.
-// So we also run `npm install`, which does this update.
+// So we also run `pnpm install`, which does this update.
 // This is a workaround until this is handled automatically by `changeset version`.
 // See https://github.com/changesets/changesets/issues/421.
 
