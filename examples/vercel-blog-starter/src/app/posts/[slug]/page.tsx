a7da35846b7da0dd0e60373d016e676e26d55e1c
shashankboosi <shashank1872@gmail.com>
2024-10-12 13:28:16 +1100

    Fix the code
    using pnpm `pnpm run code:fixes`

--- examples/vercel-blog-starter/src/app/posts/[slug]/page.tsx
+++ examples/vercel-blog-starter/src/app/posts/[slug]/page.tsx
@@ -24,12 +24,7 @@ export default async function Post({ params }: Params) {
       <Container>
         <Header />
         <article className="mb-32">
-          <PostHeader
-            title={post.title}
-            coverImage={post.coverImage}
-            date={post.date}
-            author={post.author}
-          />
+          <PostHeader title={post.title} coverImage={post.coverImage} date={post.date} author={post.author} />
           <PostBody content={content} />
         </article>
       </Container>
