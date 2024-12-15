181910cd2af2968ba31db89011a6ae04afd62316
James <james@eli.cx>
2024-12-15 23:11:57 +0000

    extract env vars from file system

---
+++ packages/cloudflare/src/cli/build/utils/extract-project-env-vars.ts
@@ -0,0 +1,38 @@
+import * as fs from "node:fs";
+import * as path from "node:path";
+
+import { parse } from "@dotenvx/dotenvx";
+import type { BuildOptions } from "@opennextjs/aws/build/helper.js";
+
+function readEnvFiles(fileNames: string[], { monorepoRoot, appPath }: BuildOptions) {
+  return fileNames
+    .flatMap((fileName) => [
+      ...(monorepoRoot !== appPath ? [path.join(monorepoRoot, fileName)] : []),
+      path.join(appPath, fileName),
+    ])
+    .filter((filePath) => fs.statSync(filePath, { throwIfNoEntry: false })?.isFile())
+    .map((filePath) => parse(fs.readFileSync(filePath).toString()))
+    .reduce<Record<string, string>>((acc, overrides) => ({ ...acc, ...overrides }), {});
+}
+
+/**
+ * Extracts the environment variables defined in various .env files for a project.
+ *
+ * The `NEXTJS_ENV` environment variable in `.dev.vars` determines the mode.
+ *
+ * Merged variables respect the following priority order.
+ * 1. `.env.{mode}.local`
+ * 2. `.env.local`
+ * 3. `.env.{mode}`
+ * 4. `.env`
+ *
+ * In a monorepo, the env files in an app's directory will take precedence over
+ * the env files at the root of the monorepo.
+ */
+export function extractProjectEnvVars(options: BuildOptions) {
+  const wranglerDevVars = readEnvFiles([".dev.vars"], options);
+
+  const mode = wranglerDevVars["NEXTJS_ENV"] ?? "production";
+
+  return readEnvFiles([".env", `.env.${mode}`, ".env.local", `.env.${mode}.local`], options);
+}
