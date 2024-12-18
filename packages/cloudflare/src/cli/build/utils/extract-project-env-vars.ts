bf6278840aaf8ba94e1653014d9f6d2ba466e8ab
James <james@eli.cx>
2024-12-18 16:42:07 +0000

    change function to read a single file

--- packages/cloudflare/src/cli/build/utils/extract-project-env-vars.ts
+++ packages/cloudflare/src/cli/build/utils/extract-project-env-vars.ts
@@ -4,14 +4,10 @@ import * as path from "node:path";
 import { parse } from "@dotenvx/dotenvx";
 import type { BuildOptions } from "@opennextjs/aws/build/helper.js";
 
-function readEnvFiles(fileNames: string[], { monorepoRoot, appPath }: BuildOptions) {
-  return fileNames
-    .flatMap((fileName) => [
-      ...(monorepoRoot !== appPath ? [path.join(monorepoRoot, fileName)] : []),
-      path.join(appPath, fileName),
-    ])
-    .filter((filePath) => fs.existsSync(filePath) && fs.statSync(filePath).isFile())
-    .map((filePath) => parse(fs.readFileSync(filePath).toString()));
+function readEnvFile(filePath: string) {
+  if (fs.existsSync(filePath) && fs.statSync(filePath).isFile()) {
+    return parse(fs.readFileSync(filePath).toString());
+  }
 }
 
 /**
@@ -30,8 +26,12 @@ function readEnvFiles(fileNames: string[], { monorepoRoot, appPath }: BuildOptio
  * In a monorepo, the env files in an app's directory will take precedence over
  * the env files at the root of the monorepo.
  */
-export function extractProjectEnvVars(mode: string, options: BuildOptions) {
-  const envVars = readEnvFiles([".env", `.env.${mode}`, ".env.local", `.env.${mode}.local`], options);
-
-  return envVars.reduce<Record<string, string>>((acc, overrides) => ({ ...acc, ...overrides }), {});
+export function extractProjectEnvVars(mode: string, { monorepoRoot, appPath }: BuildOptions) {
+  return [".env", `.env.${mode}`, ".env.local", `.env.${mode}.local`]
+    .flatMap((fileName) => [
+      ...(monorepoRoot !== appPath ? [path.join(monorepoRoot, fileName)] : []),
+      path.join(appPath, fileName),
+    ])
+    .map((fileName) => readEnvFile(fileName))
+    .reduce<Record<string, string>>((acc, overrides) => ({ ...acc, ...overrides }), {});
 }
