08cc7ec0f5c64519f14da07cbefabf5bbbf2d3f9
Dario Piotrowicz <dario@cloudflare.com>
2024-12-23 10:45:31 +0100

    add kv cache to default open-next config as requested

--- packages/cloudflare/templates/defaults/open-next.config.ts
+++ packages/cloudflare/templates/defaults/open-next.config.ts
@@ -1,11 +1,13 @@
 // default open-next.config.ts file created by @opennextjs/cloudflare
 
+import cache from "@opennextjs/cloudflare/kvCache";
+
 const config = {
   default: {
     override: {
       wrapper: "cloudflare-node",
       converter: "edge",
-      incrementalCache: "dummy",
+      incrementalCache: async () => cache,
       tagCache: "dummy",
       queue: "dummy",
     },
