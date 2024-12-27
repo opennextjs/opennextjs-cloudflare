12a1f7502972a214890365b4441bab37c0c41a65
Dario Piotrowicz <dario@cloudflare.com>
2024-12-27 15:08:13 +0100

    update location of output path in success message (#208)

--- packages/cloudflare/src/cli/build/patches/investigated/copy-package-cli-files.ts
+++ packages/cloudflare/src/cli/build/patches/investigated/copy-package-cli-files.ts
@@ -4,11 +4,12 @@ import path from "node:path";
 import type { BuildOptions } from "@opennextjs/aws/build/helper.js";
 
 import { Config } from "../../../config.js";
+import { getOutputWorkerPath } from "../../bundle-server.js";
 
 /**
  * Copies the template files present in the cloudflare adapter package into the standalone node_modules folder
  */
-export function copyPackageCliFiles(packageDistDir: string, config: Config, openNextConfig: BuildOptions) {
+export function copyPackageCliFiles(packageDistDir: string, config: Config, openNextOptions: BuildOptions) {
   console.log("# copyPackageTemplateFiles");
   const sourceDir = path.join(packageDistDir, "cli");
   const destinationDir = path.join(config.paths.internal.package, "cli");
@@ -17,6 +18,6 @@ export function copyPackageCliFiles(packageDistDir: string, config: Config, open
 
   fs.copyFileSync(
     path.join(packageDistDir, "cli", "templates", "worker.js"),
-    path.join(openNextConfig.outputDir, "worker.js")
+    getOutputWorkerPath(openNextOptions)
   );
 }
