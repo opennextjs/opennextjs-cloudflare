b38fa7126f2d32350d160910d401b2ae8c9fd68d
Dario Piotrowicz <dario@cloudflare.com>
2024-12-27 23:39:33 +0100

    implement simple unit tetsts

--- packages/cloudflare/src/cli/build/utils/normalize-path-for-inline-code.spec.ts
+++ packages/cloudflare/src/cli/build/utils/normalize-path-for-inline-code.spec.ts
@@ -3,8 +3,27 @@ import { describe, expect, it } from "vitest";
 import { normalizePathForInlineCode } from "./normalize-path-for-inline-code";
 
 describe("normalizePathForInlineCode", () => {
-  it("should extract production env vars", () => {
-    const result = normalizePathForInlineCode("TODO");
-    expect(result).toBeFalsy();
+  describe.runIf(process.platform === "win32")("windows", () => {
+    it("should produce an absolute path ready to be embedded in inlined code", () => {
+      const code = `const d = require('${normalizePathForInlineCode("/Users/me/projects/cloudflare/index.mjs")}').default;`;
+      expect(code).toEqual("const d = require('\\Users\\me\\projects\\cloudflare\\index.mjs').default;");
+    });
+
+    it("should produce a relative path ready to be embedded in inlined code", () => {
+      const code = `const d = require('${normalizePathForInlineCode("../../tmp/index.mjs")}').default;`;
+      expect(code).toEqual("const d = require('..\\..\\tmp\\index.mjs').default;");
+    });
+  });
+
+  describe.runIf(process.platform === "linux" || process.platform === "darwin")("linux/mac", () => {
+    it("should produce an absolute path ready to be embedded in inlined code", () => {
+      const code = `const d = require('${normalizePathForInlineCode("/Users/me/projects/cloudflare/index.mjs")}').default;`;
+      expect(code).toEqual("const d = require('/Users/me/projects/cloudflare/index.mjs').default;");
+    });
+
+    it("should produce a relative path ready to be embedded in inlined code", () => {
+      const code = `const d = require('${normalizePathForInlineCode("../../tmp/index.mjs")}').default;`;
+      expect(code).toEqual("const d = require('../../tmp/index.mjs').default;");
+    });
   });
 });
