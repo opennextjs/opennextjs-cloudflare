b6964c192d874503b2136f83205b4abaf1e673e8
Dario Piotrowicz <dario@cloudflare.com>
2025-01-02 18:19:25 +0100

    fixup! create a wrangler.toml file for the user in case one is not already present
    update outdated code comment

--- packages/cloudflare/src/cli/build/index.ts
+++ packages/cloudflare/src/cli/build/index.ts
@@ -184,9 +184,10 @@ function ensureCloudflareConfig(config: OpenNextConfig) {
 }
 
 /**
- * Creates a `wrangler.jsonc` file for the user if it doesn't exist, but only after asking for the user's confirmation.
+ * Creates a `wrangler.jsonc` file for the user if a wrangler config file doesn't already exist,
+ * but only after asking for the user's confirmation.
  *
- * If the user refuses an error is thrown (since the file is mandatory).
+ * If the user refuses a warning is shown (which offers ways to opt out of this check to the user).
  *
  * @param projectOpts The options for the project
  */
