36c8c5a3612f067768fc3a8b79e12beac2045331
James <james@eli.cx>
2025-01-09 10:37:20 +0000

    rename to `ensureNextjsVersionSupported`

--- packages/cloudflare/src/cli/build/index.ts
+++ packages/cloudflare/src/cli/build/index.ts
@@ -55,7 +55,7 @@ export async function build(projectOpts: ProjectOptions): Promise<void> {
   buildHelper.checkRunningInsideNextjsApp(options);
   logger.info(`App directory: ${options.appPath}`);
   buildHelper.printNextjsVersion(options);
-  showErrorForOldNextjsVersions(options);
+  ensureNextjsVersionSupported(options);
   buildHelper.printOpenNextVersion(options);
 
   if (projectOpts.skipNextBuild) {
@@ -278,7 +278,7 @@ export async function getLatestCompatDate(): Promise<string | undefined> {
   }
 }
 
-function showErrorForOldNextjsVersions(options: buildHelper.BuildOptions) {
+function ensureNextjsVersionSupported(options: buildHelper.BuildOptions) {
   if (buildHelper.compareSemver(options.nextVersion, "14.0.0") < 0) {
     logger.error("Next.js version unsupported, please upgrade to version 14 or greater.");
     process.exit(1);
