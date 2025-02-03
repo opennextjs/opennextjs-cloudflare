714172d18b988f113ebb2d0a5b5454a29460ac45
Victor Berchet <victor@suumit.com>
2025-02-03 13:55:35 +0100

    fix: trailing slash redirect (#329)
    fixes #312

---
+++ .changeset/nice-penguins-boil.md
@@ -0,0 +1,5 @@
+---
+"@opennextjs/cloudflare": patch
+---
+
+fix: trailing slash redirect
--- examples/e2e/app-router/e2e/trailing.test.ts
+++ examples/e2e/app-router/e2e/trailing.test.ts
@@ -1,15 +1,13 @@
 import { expect, test } from "@playwright/test";
 
-// trailingSlash redirecting doesn't work: https://github.com/opennextjs/opennextjs-cloudflare/issues/312
-test.skip("trailingSlash redirect", async ({ page }) => {
+test("trailingSlash redirect", async ({ page }) => {
   const response = await page.goto("/ssr/");
 
   expect(response?.request().redirectedFrom()?.url()).toMatch(/\/ssr\/$/);
   expect(response?.request().url()).toMatch(/\/ssr$/);
 });
 
-// trailingSlash redirecting doesn't work: https://github.com/opennextjs/opennextjs-cloudflare/issues/312
-test.skip("trailingSlash redirect with search parameters", async ({ page }) => {
+test("trailingSlash redirect with search parameters", async ({ page }) => {
   const response = await page.goto("/ssr/?happy=true");
 
   expect(response?.request().redirectedFrom()?.url()).toMatch(/\/ssr\/\?happy=true$/);
