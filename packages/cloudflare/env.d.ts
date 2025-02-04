a1fcbb6e68a45789e41efab5130db33c8c84dc11
Victor Berchet <victor@suumit.com>
2025-02-04 15:53:42 +0100

    refactor: Next env loading (#337)

--- packages/cloudflare/env.d.ts
+++ packages/cloudflare/env.d.ts
@@ -7,8 +7,6 @@ declare global {
       NEXT_PRIVATE_DEBUG_CACHE?: string;
       OPEN_NEXT_ORIGIN: string;
       NODE_ENV?: string;
-      // Whether process.env has been populated (on first request).
-      __PROCESS_ENV_POPULATED?: string;
     }
   }
 }
