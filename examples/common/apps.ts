1fd08b3a03b88e91278941d7bbd24fbfb05e2133
Victor Berchet <victor@suumit.com>
2025-02-05 11:02:52 +0100

    refactor: simplify the e2e and script configs (#342)

---
+++ examples/common/apps.ts
@@ -0,0 +1,44 @@
+// List of all e2e tested apps
+const apps = [
+  // examples
+  "api",
+  "create-next-app",
+  "middleware",
+  "vercel-blog-starter",
+  "vercel-commerce",
+  // e2e
+  "app-pages-router",
+  "app-router",
+  "pages-router",
+  // bugs
+  "gh-119",
+  "gh-219",
+  "gh-223",
+] as const;
+
+export type AppName = (typeof apps)[number];
+
+const BASE_WRANGLER_PORT = 8770;
+const BASE_NEXT_PORT = 3100;
+
+/**
+ * Returns a distinct port for each application so they can run in parallel.
+ */
+export function getAppPort(app: AppName, { isWorker = true } = {}) {
+  const index = apps.indexOf(app);
+  if (index === -1) {
+    throw new Error(`Unknown app: ${app}`);
+  }
+  return isWorker ? BASE_WRANGLER_PORT + index : BASE_NEXT_PORT + index;
+}
+
+/**
+ * Returns a distinct port for each application so they can run in parallel.
+ */
+export function getInspectorPort(app: AppName) {
+  const index = apps.indexOf(app);
+  if (index === -1) {
+    throw new Error(`Unknown app: ${app}`);
+  }
+  return 9300 + index;
+}
---
+++ examples/common/config-e2e.ts
@@ -0,0 +1,90 @@
+import { defineConfig, devices } from "@playwright/test";
+import type nodeProcess from "node:process";
+import { getAppPort, getInspectorPort, type AppName } from "./apps";
+
+declare const process: typeof nodeProcess;
+
+export function configurePlaywright(
+  app: AppName,
+  {
+    // Do we run on CI?
+    isCI = false,
+    // Do we run on workers (`wrangler dev`) or on Node (`next dev`)
+    isWorker = true,
+    // Tests with multiple browsers
+    multipleBrowsers = true,
+  } = {}
+) {
+  const port = getAppPort(app, { isWorker });
+  const inspectorPort = getInspectorPort(app);
+  const baseURL = `http://localhost:${port}`;
+  let command: string;
+  let timeout: number;
+  if (isWorker) {
+    if (isCI) {
+      // Do not build on CI - there is a preceding build step
+      command = `pnpm wrangler dev --port ${port} --inspector-port ${inspectorPort}`;
+      timeout = 100_000;
+    } else {
+      timeout = 500_000;
+      command = `pnpm preview --port ${port}`;
+    }
+  } else {
+    timeout = 100_000;
+    command = `pnpm dev --port ${port}`;
+  }
+
+  const projects = [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+  ];
+  if (!multipleBrowsers) {
+    projects.length = 1;
+  }
+
+  /**
+   * See https://playwright.dev/docs/test-configuration.
+   */
+  return defineConfig({
+    testDir: "./",
+    /* ignore runtime specific tests */
+    testIgnore: isWorker ? "*next.spec.ts" : "*cloudflare.spec.ts",
+    /* Run tests in files in parallel */
+    fullyParallel: true,
+    /* Fail the build on CI if you accidentally left test.only in the source code. */
+    forbidOnly: isCI,
+    /* Retry on CI only */
+    retries: isCI ? 2 : 0,
+    /* Opt out of parallel tests on CI. */
+    workers: isCI ? 1 : undefined,
+    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+    reporter: "html",
+    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+    use: {
+      /* Base URL to use in actions like `await page.goto('/')`. */
+      baseURL,
+      /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+      trace: "on-first-retry",
+    },
+
+    projects,
+
+    /* Run your local dev server before starting the tests */
+    webServer: {
+      command,
+      url: baseURL,
+      reuseExistingServer: !isCI,
+      timeout,
+    },
+  });
+}
