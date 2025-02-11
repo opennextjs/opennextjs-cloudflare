27c6b556f142cd71b86290b0168d00454040b957
James <james@eli.cx>
2025-02-11 07:59:53 +0000

    split up tests

--- packages/cloudflare/src/api/memory-queue.spec.ts
+++ packages/cloudflare/src/api/memory-queue.spec.ts
@@ -1,7 +1,7 @@
 import { generateMessageGroupId } from "@opennextjs/aws/core/routing/queue.js";
-import { beforeAll, describe, expect, it, vi } from "vitest";
+import { afterEach, beforeAll, describe, expect, it, vi } from "vitest";
 
-import cache from "./memory-queue";
+import cache, { DEFAULT_REVALIDATION_TIMEOUT } from "./memory-queue";
 
 vi.mock("./.next/prerender-manifest.json", () => Promise.resolve({ preview: { previewModeId: "id" } }));
 
@@ -17,20 +17,36 @@ describe("MemoryQueue", () => {
     globalThis.internalFetch = vi.fn().mockReturnValue(new Promise((res) => setTimeout(() => res(true), 1)));
   });
 
-  it("should de-dupe revalidations", async () => {
-    const firstBatch = [cache.send(defaultOpts), cache.send(defaultOpts)];
-    vi.advanceTimersByTime(1);
-    await Promise.all(firstBatch);
+  afterEach(() => vi.clearAllMocks());
+
+  it("should process revalidations for a path", async () => {
+    const firstRequest = cache.send(defaultOpts);
+    vi.advanceTimersByTime(DEFAULT_REVALIDATION_TIMEOUT);
+    await firstRequest;
     expect(globalThis.internalFetch).toHaveBeenCalledTimes(1);
 
-    const secondBatch = [cache.send(defaultOpts)];
-    vi.advanceTimersByTime(10_000);
-    await Promise.all(secondBatch);
-    expect(globalThis.internalFetch).toHaveBeenCalledTimes(2);
-
-    const thirdBatch = [cache.send({ ...defaultOpts, MessageGroupId: generateMessageGroupId("/other") })];
+    const secondRequest = cache.send(defaultOpts);
     vi.advanceTimersByTime(1);
-    await Promise.all(thirdBatch);
-    expect(globalThis.internalFetch).toHaveBeenCalledTimes(3);
+    await secondRequest;
+    expect(globalThis.internalFetch).toHaveBeenCalledTimes(2);
+  });
+
+  it("should process revalidations for multiple paths", async () => {
+    const firstRequest = cache.send(defaultOpts);
+    vi.advanceTimersByTime(1);
+    await firstRequest;
+    expect(globalThis.internalFetch).toHaveBeenCalledTimes(1);
+
+    const secondRequest = cache.send({ ...defaultOpts, MessageGroupId: generateMessageGroupId("/other") });
+    vi.advanceTimersByTime(1);
+    await secondRequest;
+    expect(globalThis.internalFetch).toHaveBeenCalledTimes(2);
+  });
+
+  it("should de-dupe revalidations", async () => {
+    const requests = [cache.send(defaultOpts), cache.send(defaultOpts)];
+    vi.advanceTimersByTime(1);
+    await Promise.all(requests);
+    expect(globalThis.internalFetch).toHaveBeenCalledTimes(1);
   });
 });
