c5e9bffcc6b6a1dd58f370ffd7d74c8d87a9d6a3
James Anderson <james@eli.cx>
2025-02-18 07:55:45 +0000

    chore: unskip more isr e2es (#390)

--- examples/e2e/pages-router/open-next.config.ts
+++ examples/e2e/pages-router/open-next.config.ts
@@ -1,14 +1,16 @@
 import type { OpenNextConfig } from "@opennextjs/aws/types/open-next.js";
+import kvCache from "@opennextjs/cloudflare/kv-cache";
+import memoryQueue from "@opennextjs/cloudflare/memory-queue";
 
 const config: OpenNextConfig = {
   default: {
     override: {
       wrapper: "cloudflare-node",
       converter: "edge",
+      incrementalCache: () => kvCache,
+      queue: () => memoryQueue,
       // Unused implementation
-      incrementalCache: "dummy",
       tagCache: "dummy",
-      queue: "dummy",
     },
   },
 
