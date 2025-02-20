db8969b6a9fec3ec61f9e13e583dc77910118470
Victor Berchet <victor@suumit.com>
2025-02-20 13:48:23 +0100

    test: describe playwright tests (#402)

--- examples/bugs/gh-119/e2e/base.spec.ts
+++ examples/bugs/gh-119/e2e/base.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from "@playwright/test";
 
-test("the index page of the application shows the Next.js logo", async ({ page }) => {
-  await page.goto("/");
-  await expect(page.getByAltText("Next.js logo")).toBeVisible();
+test.describe("bugs/gh-119", () => {
+  test("the index page of the application shows the Next.js logo", async ({ page }) => {
+    await page.goto("/");
+    await expect(page.getByAltText("Next.js logo")).toBeVisible();
+  });
 });
--- examples/middleware/e2e/base.spec.ts
+++ examples/middleware/e2e/base.spec.ts
@@ -1,37 +1,39 @@
 import { test, expect } from "@playwright/test";
 
-test("redirect", async ({ page }) => {
-  await page.goto("/");
-  await page.click('[href="/about"]');
-  await page.waitForURL("**/redirected");
-  expect(await page.textContent("h1")).toContain("Redirected");
-});
+test.describe("middleware", () => {
+  test("redirect", async ({ page }) => {
+    await page.goto("/");
+    await page.click('[href="/about"]');
+    await page.waitForURL("**/redirected");
+    expect(await page.textContent("h1")).toContain("Redirected");
+  });
 
-test("rewrite", async ({ page }) => {
-  await page.goto("/");
-  await page.click('[href="/another"]');
-  await page.waitForURL("**/another");
-  expect(await page.textContent("h1")).toContain("Rewrite");
-});
+  test("rewrite", async ({ page }) => {
+    await page.goto("/");
+    await page.click('[href="/another"]');
+    await page.waitForURL("**/another");
+    expect(await page.textContent("h1")).toContain("Rewrite");
+  });
 
-test("no matching middleware", async ({ page }) => {
-  await page.goto("/");
-  await page.click('[href="/about2"]');
-  await page.waitForURL("**/about2");
-  expect(await page.textContent("h1")).toContain("About 2");
-});
+  test("no matching middleware", async ({ page }) => {
+    await page.goto("/");
+    await page.click('[href="/about2"]');
+    await page.waitForURL("**/about2");
+    expect(await page.textContent("h1")).toContain("About 2");
+  });
 
-test("matching noop middleware", async ({ page }) => {
-  await page.goto("/");
-  await page.click('[href="/middleware"]');
-  await page.waitForURL("**/middleware");
-  expect(await page.textContent("h1")).toContain("Via middleware");
-});
+  test("matching noop middleware", async ({ page }) => {
+    await page.goto("/");
+    await page.click('[href="/middleware"]');
+    await page.waitForURL("**/middleware");
+    expect(await page.textContent("h1")).toContain("Via middleware");
+  });
 
-// Test for https://github.com/opennextjs/opennextjs-cloudflare/issues/201
-test("clerk middleware", async ({ page }) => {
-  const res = await page.request.post("/clerk", { data: "some body" });
-  expect(res.ok()).toEqual(true);
-  expect(res.status()).toEqual(200);
-  await expect(res.text()).resolves.toEqual("Hello clerk");
+  // Test for https://github.com/opennextjs/opennextjs-cloudflare/issues/201
+  test("clerk middleware", async ({ page }) => {
+    const res = await page.request.post("/clerk", { data: "some body" });
+    expect(res.ok()).toEqual(true);
+    expect(res.status()).toEqual(200);
+    await expect(res.text()).resolves.toEqual("Hello clerk");
+  });
 });
