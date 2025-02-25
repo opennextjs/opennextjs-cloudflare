aa4d8fa08c377f3990feb27bcde92b5aa2895042
Dario Piotrowicz <dario@cloudflare.com>
2025-02-25 10:31:06 +0000

    clean up load-instrumentation spec file

--- packages/cloudflare/src/cli/build/patches/plugins/load-instrumentation.spec.ts
+++ packages/cloudflare/src/cli/build/patches/plugins/load-instrumentation.spec.ts
@@ -14,87 +14,48 @@ vi.mock(import("node:fs"), async (importOriginal) => {
 });
 
 describe("LoadInstrumentationModule", () => {
-  test("patch when an instrumentation file is not present", async () => {
-    const code = `
-export default class NextNodeServer extends BaseServer<
-  Options,
-  NodeNextRequest,
-  NodeNextResponse
-> {
-  protected async loadInstrumentationModule() {
-    if (!this.serverOptions.dev) {
-      try {
-        this.instrumentation = await dynamicRequire(
-          resolve(
-            this.serverOptions.dir || '.',
-            this.serverOptions.conf.distDir!,
-            'server',
-            INSTRUMENTATION_HOOK_FILENAME
-          )
-        )
-      } catch (err: any) {
-        if (err.code !== 'MODULE_NOT_FOUND') {
-          throw new Error(
-            'An error occurred while loading the instrumentation hook',
-            { cause: err }
-          )
+  const code = `
+      export default class NextNodeServer extends BaseServer {
+        protected async loadInstrumentationModule() {
+          if (!this.serverOptions.dev) {
+            try {
+              this.instrumentation = await dynamicRequire(
+                resolve(
+                  this.serverOptions.dir || '.',
+                  this.serverOptions.conf.distDir!,
+                  'server',
+                  INSTRUMENTATION_HOOK_FILENAME
+                )
+              )
+            } catch (err: any) {
+              if (err.code !== 'MODULE_NOT_FOUND') {
+                throw new Error(
+                  'An error occurred while loading the instrumentation hook',
+                  { cause: err }
+                )
+              }
+            }
+          }
+          return this.instrumentation
         }
       }
-    }
-    return this.instrumentation
-  }
-}`;
+    `;
 
+  test("patch when an instrumentation file is not present", async () => {
     expect(patchCode(code, await getRule(null))).toMatchInlineSnapshot(`
-      "export default class NextNodeServer extends BaseServer<
-        Options,
-        NodeNextRequest,
-        NodeNextResponse
-      > {
-        async loadInstrumentationModule() { this.instrumentation = null; return this.instrumentation; }
-      }"
-    `);
+        "export default class NextNodeServer extends BaseServer {
+                async loadInstrumentationModule() { this.instrumentation = null; return this.instrumentation; }
+              }
+            "
+      `);
   });
 
   test("patch when an instrumentation file is present", async () => {
-    const code = `
-export default class NextNodeServer extends BaseServer<
-  Options,
-  NodeNextRequest,
-  NodeNextResponse
-> {
-  protected async loadInstrumentationModule() {
-    if (!this.serverOptions.dev) {
-      try {
-        this.instrumentation = await dynamicRequire(
-          resolve(
-            this.serverOptions.dir || '.',
-            this.serverOptions.conf.distDir!,
-            'server',
-            INSTRUMENTATION_HOOK_FILENAME
-          )
-        )
-      } catch (err: any) {
-        if (err.code !== 'MODULE_NOT_FOUND') {
-          throw new Error(
-            'An error occurred while loading the instrumentation hook',
-            { cause: err }
-          )
-        }
-      }
-    }
-    return this.instrumentation
-  }
-}`;
-
     expect(patchCode(code, await getRule("/_file_exists_/instrumentation.js"))).toMatchInlineSnapshot(`
-      "export default class NextNodeServer extends BaseServer<
-        Options,
-        NodeNextRequest,
-        NodeNextResponse
-      > {
-        async loadInstrumentationModule() { this.instrumentation = require('/_file_exists_/instrumentation.js'); return this.instrumentation; }
-      }"
-    `);
+        "export default class NextNodeServer extends BaseServer {
+                async loadInstrumentationModule() { this.instrumentation = require('/_file_exists_/instrumentation.js'); return this.instrumentation; }
+              }
+            "
+      `);
   });
 });
