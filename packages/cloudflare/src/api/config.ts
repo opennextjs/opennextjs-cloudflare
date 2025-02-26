36cb6d081a762a5ea748392584fc35c5277725cc
Dario Piotrowicz <dario@cloudflare.com>
2025-02-26 10:26:19 +0000

    improve jsdoc comments

--- packages/cloudflare/src/api/config.ts
+++ packages/cloudflare/src/api/config.ts
@@ -3,22 +3,50 @@ import type { IncrementalCache, Queue, TagCache } from "@opennextjs/aws/types/ov
 
 export type CloudflareConfigOptions = {
   /**
-   * The incremental cache implementation to use
+   * The incremental cache implementation to use (for more details see the [Incremental Cache documentation](https://opennext.js.org/aws/config/overrides/incremental_cache))
    *
-   * see: https://opennext.js.org/aws/config/overrides/incremental_cache
+   * `@opennextjs/cloudflare` offers a kv incremental cache implementation ready
+   * to use which can be imported from `"@opennextjs/cloudflare/kv-cache"`
+   *
+   * @example
+   *   import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
+   *   import cache from "@opennextjs/cloudflare/kv-cache";
+   *
+   *   export default defineCloudflareConfig({
+   *     incrementalCache: cache,
+   *   });
    */
   incrementalCache?: IncrementalCache | (() => IncrementalCache | Promise<IncrementalCache>);
+
   /**
-   * The tag cache implementation to use
+   * The tag cache implementation to use (for more details see the [Tag Cache documentation](https://opennext.js.org/aws/config/overrides/tag_cache))
    *
-   * see: https://opennext.js.org/aws/config/overrides/tag_cache
+   * `@opennextjs/cloudflare` offers a d1 tag cache implementation ready
+   * to use which can be imported from `"@opennextjs/cloudflare/d1-tag-cache"`
+   *
+   * @example
+   *   import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
+   *   import cache from "@opennextjs/cloudflare/d1-tag-cache";
+   *
+   *   export default defineCloudflareConfig({
+   *     tagCache: cache,
+   *   });
    */
   tagCache?: TagCache | (() => TagCache | Promise<TagCache>);
 
   /**
-   * The revalidation queue implementation to use
+   * The revalidation queue implementation to use (for more details see the [Queue documentation](https://opennext.js.org/aws/config/overrides/queue))
    *
-   * see: https://opennext.js.org/aws/config/overrides/queue
+   * `@opennextjs/cloudflare` offers an in memory queue implementation ready
+   * to use which can be imported from `"@opennextjs/cloudflare/memory-queue"`
+   *
+   * @example
+   *   import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
+   *   import memoryQueue from "@opennextjs/cloudflare/memory-queue";
+   *
+   *   export default defineCloudflareConfig({
+   *     queue: memoryQueue,
+   *   });
    */
   queue?: Queue | (() => Queue | Promise<Queue>);
 };
