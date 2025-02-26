fe3a51b2d08cfa0b3db3b531e8ec3deae19f1a25
Dario Piotrowicz <dario@cloudflare.com>
2025-02-26 10:28:55 +0000

    update some missing `open-next.config.ts` files

--- examples/e2e/app-router/open-next.config.ts
+++ examples/e2e/app-router/open-next.config.ts
@@ -1,27 +1,10 @@
-import type { OpenNextConfig } from "@opennextjs/aws/types/open-next.js";
+import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
 import tagCache from "@opennextjs/cloudflare/d1-tag-cache";
 import incrementalCache from "@opennextjs/cloudflare/kv-cache";
 import memoryQueue from "@opennextjs/cloudflare/memory-queue";
 
-const config: OpenNextConfig = {
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      incrementalCache: async () => incrementalCache,
-      tagCache: () => tagCache,
-      queue: () => memoryQueue,
-    },
-  },
-
-  middleware: {
-    external: true,
-    override: {
-      wrapper: "cloudflare-edge",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-    },
-  },
-};
-
-export default config;
+export default defineCloudflareConfig({
+  incrementalCache,
+  tagCache,
+  queue: memoryQueue,
+});
--- examples/vercel-commerce/open-next.config.ts
+++ examples/vercel-commerce/open-next.config.ts
@@ -1,25 +1,3 @@
-import type { OpenNextConfig } from '@opennextjs/aws/types/open-next';
+import { defineCloudflareConfig } from '@opennextjs/cloudflare/config';
 
-const config: OpenNextConfig = {
-  default: {
-    override: {
-      wrapper: 'cloudflare-node',
-      converter: 'edge',
-      // Unused implementation
-      incrementalCache: 'dummy',
-      tagCache: 'dummy',
-      queue: 'dummy'
-    }
-  },
-
-  middleware: {
-    external: true,
-    override: {
-      wrapper: 'cloudflare-edge',
-      converter: 'edge',
-      proxyExternalRequest: 'fetch'
-    }
-  }
-};
-
-export default config;
+export default defineCloudflareConfig();
