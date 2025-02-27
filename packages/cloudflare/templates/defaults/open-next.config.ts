53e43d200cb3a7e6e5940f4147e050f6694cbef7
Dario Piotrowicz <dario@cloudflare.com>
2025-02-27 19:07:10 +0000

    update last missing import

--- packages/cloudflare/templates/defaults/open-next.config.ts
+++ packages/cloudflare/templates/defaults/open-next.config.ts
@@ -1,7 +1,7 @@
 // default open-next.config.ts file created by @opennextjs/cloudflare
 import { defineCloudflareConfig } from "@opennextjs/cloudflare/dist/api/config";
-import cache from "@opennextjs/cloudflare/dist/api/kv-cache";
+import kvIncrementalCache from "@opennextjs/cloudflare/dist/api/kv-cache";
 
 export default defineCloudflareConfig({
-  incrementalCache: cache,
+  incrementalCache: kvIncrementalCache,
 });
