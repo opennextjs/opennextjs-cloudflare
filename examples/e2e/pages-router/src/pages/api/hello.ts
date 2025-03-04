95cd728e323ab2a3e09fae50c95168cffc552255
Dorseuil Nicolas <nicodorseuil@yahoo.fr>
2025-03-04 14:14:56 +0100

    sync with aws

--- examples/e2e/pages-router/src/pages/api/hello.ts
+++ examples/e2e/pages-router/src/pages/api/hello.ts
@@ -1,10 +1,13 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// Next.js API route support: https://nextjs.org/docs/pages/building-your-application/routing/api-routes
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
   hello: string;
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  res.status(200).json({ hello: "world" });
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  res.status(200).json({ hello: "OpenNext rocks!" });
 }
