e823060e71f43f31540d0ca7c9756f455555ea32
Dorseuil Nicolas <nicodorseuil@yahoo.fr>
2025-03-13 10:20:37 +0100

    Handle failure and add some test

--- packages/cloudflare/src/api/durable-objects/queue.ts
+++ packages/cloudflare/src/api/durable-objects/queue.ts
@@ -1,6 +1,6 @@
 import { error } from "@opennextjs/aws/adapters/logger.js";
 import type { QueueMessage } from "@opennextjs/aws/types/overrides";
-import { IgnorableError } from "@opennextjs/aws/utils/error.js";
+import {  FatalError, IgnorableError, isOpenNextError, RecoverableError } from "@opennextjs/aws/utils/error.js";
 import { DurableObject } from "cloudflare:workers";
 
 const MAX_REVALIDATION_BY_DURABLE_OBJECT = 5;
@@ -16,9 +16,12 @@ export class DurableObjectQueueHandler extends DurableObject<CloudflareEnv> {
   // TODO: handle incremental cache with only eventual consistency (i.e. KV or R2/D1 with the optional cache layer on top)
   ongoingRevalidations = new Map<string, Promise<void>>();
 
+  // TODO: restore the state of the failed revalidations - Probably in the next PR where i'll add the storage
+  routeInFailedState = new Map<string, {msg: ExtendedQueueMessage, retryCount: number, nextAlarm: number}>();
+
   service: NonNullable<CloudflareEnv["NEXT_CACHE_REVALIDATION_WORKER"]>;
 
-  // TODO: allow this to be configurable
+  // TODO: allow this to be configurable - How do we want todo that? env variable? passed down from the queue override ?
   maxRevalidations = MAX_REVALIDATION_BY_DURABLE_OBJECT;
 
   constructor(ctx: DurableObjectState, env: CloudflareEnv) {
@@ -34,8 +37,14 @@ export class DurableObjectQueueHandler extends DurableObject<CloudflareEnv> {
     // If there is already an ongoing revalidation, we don't need to revalidate again
     if (this.ongoingRevalidations.has(msg.MessageDeduplicationId)) return;
 
+    // The route is already in a failed state, it will be retried later
+    if (this.routeInFailedState.has(msg.MessageDeduplicationId)) return;
+
     if(this.ongoingRevalidations.size >= MAX_REVALIDATION_BY_DURABLE_OBJECT) {
       const ongoingRevalidations = this.ongoingRevalidations.values()
+      // When there is more than the max revalidations, we block concurrency until one of the revalidations finishes
+      // We still await the promise to ensure the revalidation is completed
+      // This is fine because the queue itself run inside a waitUntil
       await this.ctx.blockConcurrencyWhile(() => Promise.race(ongoingRevalidations));
     }
 
@@ -47,15 +56,17 @@ export class DurableObjectQueueHandler extends DurableObject<CloudflareEnv> {
       revalidationPromise
     );
 
+    // TODO: check if the object stays up during waitUntil so that the internal state is maintained
     this.ctx.waitUntil(revalidationPromise);
   }
 
-  private async executeRevalidation({MessageBody: {host, url}, MessageDeduplicationId, previewModeId}: ExtendedQueueMessage) {
+  private async executeRevalidation(msg: ExtendedQueueMessage) {
     try {
+      const {MessageBody: {host, url}, previewModeId} = msg;
       const protocol = host.includes("localhost") ? "http" : "https";
 
       //TODO: handle the different types of errors that can occur during the fetch (i.e. timeout, network error, etc)
-      await this.service.fetch(`${protocol}://${host}${url}`, {
+      const response = await this.service.fetch(`${protocol}://${host}${url}`, {
         method: "HEAD",
         headers: {
           "x-prerender-revalidate": previewModeId,
@@ -63,11 +74,84 @@ export class DurableObjectQueueHandler extends DurableObject<CloudflareEnv> {
         },
         signal: AbortSignal.timeout(DEFAULT_REVALIDATION_TIMEOUT_MS)
       })
+      // Now we need to handle errors from the fetch
+      if(response.status === 200 && response.headers.get("x-nextjs-cache") !== "REVALIDATED") {
+        // Something is very wrong here, it means that either the page is not ISR/SSG (and we shouldn't be here) or the `x-prerender-revalidate` header is not correct (and it should not happen either)
+        throw new FatalError(`The revalidation for ${host}${url} cannot be done. This error should never happen.`);
+      } else if (response.status === 404) {
+        // The page is not found, we should not revalidate it
+        throw new IgnorableError(`The revalidation for ${host}${url} cannot be done because the page is not found. It's either expected or an error in user code itself`);
+      } else if (response.status === 500) {
+        // A server error occurred, we should retry
+
+          await this.addToFailedState(msg);
+
+        throw new IgnorableError(`Something went wrong while revalidating ${host}${url}`);
+      } else if (response.status !== 200) {
+        // TODO: check if we need to handle cloudflare specific status codes/errors
+        // An unknown error occurred, most likely from something in user code like missing auth in the middleware
+        throw new RecoverableError(`An unknown error occurred while revalidating ${host}${url}`);
+      }
     } catch (e) {
+      // Do we want to propagate the error to the calling worker?
+      if(!isOpenNextError(e)) {
+        await this.addToFailedState(msg);
+      }
       error(e);
     } finally {
-      this.ongoingRevalidations.delete(MessageDeduplicationId);
+      this.ongoingRevalidations.delete(msg.MessageDeduplicationId);
     }
   }
+
+  override async alarm() {
+    // We fetch the first event that needs to be retried or if the date is expired
+    const nextEventToRetry = Array.from(this.routeInFailedState.values()).filter((failing) => failing.nextAlarm > Date.now()).sort(({nextAlarm: a}, {nextAlarm: b}) => a - b)[0];
+    // We also have to check if there are expired events, if the revalidation takes too long, or if the 
+    const expiredEvents = Array.from(this.routeInFailedState.values()).filter(({nextAlarm}) => nextAlarm <= Date.now());
+    const allEventsToRetry = (nextEventToRetry && nextEventToRetry.nextAlarm > Date.now()) ? [nextEventToRetry, ...expiredEvents] : expiredEvents;
+    for(const event of allEventsToRetry) {
+      await this.executeRevalidation(event.msg);
+      this.routeInFailedState.delete(event.msg.MessageDeduplicationId);
+    }
+  }
+
   
+  async addToFailedState(msg: ExtendedQueueMessage) {
+    const existingFailedState = this.routeInFailedState.get(msg.MessageDeduplicationId);
+    let nextAlarm = Date.now() + 2_000;
+    
+    if(existingFailedState) {
+      if(existingFailedState.retryCount >= 6) {
+        // We give up after 6 retries and log the error
+        error(`The revalidation for ${msg.MessageBody.host}${msg.MessageBody.url} has failed after 6 retries. It will not be tried again, but subsequent ISR requests will retry.`);
+        this.routeInFailedState.delete(msg.MessageDeduplicationId);
+        return;
+      }
+      nextAlarm = Date.now() + Math.pow(2, existingFailedState.retryCount + 1) * 2_000;
+      this.routeInFailedState.set(msg.MessageDeduplicationId, {
+        ...existingFailedState,
+        retryCount: existingFailedState.retryCount + 1,
+        nextAlarm
+      });
+    } else {
+      this.routeInFailedState.set(msg.MessageDeduplicationId, {
+        msg,
+        retryCount: 1,
+        nextAlarm
+      });
+    }
+    // We probably want to do something if routeInFailedState is becoming too big, at least log it
+    await this.addAlarm();
+    
+  }
+
+  async addAlarm() {
+    const existingAlarm = await this.ctx.storage.getAlarm({allowConcurrency: false});
+    if(existingAlarm) return;
+    if(this.routeInFailedState.size === 0) return;
+    
+    const nextAlarmToSetup = Array.from(this.routeInFailedState.values()).reduce((acc, {nextAlarm}) => Math.min(acc, nextAlarm), Infinity);
+    await this.ctx.storage.setAlarm(nextAlarmToSetup);
+  }
+
 }
