039dc1b62e751e5df07821803088dd2ad697dad8
James <james@eli.cx>
2025-03-20 22:35:07 +0000

    add separate app for e2e

--- examples/e2e/app-router/open-next.config.ts
+++ examples/e2e/app-router/open-next.config.ts
@@ -1,15 +1,10 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-// import kvIncrementalCache from "@opennextjs/cloudflare/kv-cache";
+import kvIncrementalCache from "@opennextjs/cloudflare/kv-cache";
 import shardedTagCache from "@opennextjs/cloudflare/do-sharded-tag-cache";
 import doQueue from "@opennextjs/cloudflare/durable-queue";
-import r2IncrementalCache from "@opennextjs/cloudflare/r2-incremental-cache";
-import { withRegionalCache } from "@opennextjs/cloudflare/regional-cache";
 
 export default defineCloudflareConfig({
-  incrementalCache: withRegionalCache(r2IncrementalCache, {
-    mode: "long-lived",
-    shouldLazilyUpdateOnCacheHit: true,
-  }),
+  incrementalCache: kvIncrementalCache,
   tagCache: shardedTagCache({ numberOfShards: 12, regionalCache: true }),
   queue: doQueue,
 });
