bdae00eccfa27d826477b18cb14316800c482dbb
Dorseuil Nicolas <nicodorseuil@yahoo.fr>
2025-03-21 12:34:37 +0100

    double sharding

--- examples/e2e/app-router/open-next.config.ts
+++ examples/e2e/app-router/open-next.config.ts
@@ -1,11 +1,18 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-import d1TagCache from "@opennextjs/cloudflare/d1-tag-cache";
 import kvIncrementalCache from "@opennextjs/cloudflare/kv-cache";
 import doQueue from "@opennextjs/cloudflare/durable-queue";
 import shardedTagCache from "@opennextjs/cloudflare/do-sharded-tag-cache";
 
 export default defineCloudflareConfig({
   incrementalCache: kvIncrementalCache,
-  tagCache: shardedTagCache({ numberOfShards: 12}),
+  // With such a configuration, we could have up to 12 * 8 + 12 * 2 = 120 Durable Objects instances
+  tagCache: shardedTagCache({
+    numberOfShards: 12,
+    enableDoubleSharding: true,
+    doubleShardingOpts: {
+      softShards: 8,
+      hardShards: 2,
+    },
+  }),
   queue: doQueue,
 });
