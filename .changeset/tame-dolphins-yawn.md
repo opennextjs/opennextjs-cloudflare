2a0dc6f44f48d1204a6bbe51da21a69b177f948c
Victor Berchet <victor@suumit.com>
2025-03-22 14:28:37 +0100

    Drop kvCache

---
+++ .changeset/tame-dolphins-yawn.md
@@ -0,0 +1,5 @@
+---
+"@opennextjs/cloudflare": minor
+---
+
+Drop the deprecated kvCache in favor of kv-cache
--- packages/cloudflare/src/api/kvCache.ts
+++
@@ -1,6 +0,0 @@
-import kvIncrementalCache from "./kv-cache.js";
-
-/**
- * @deprecated Please import from `kv-cache` instead of `kvCache`.
- */
-export default kvIncrementalCache;
