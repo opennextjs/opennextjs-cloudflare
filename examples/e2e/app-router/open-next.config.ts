54e0d4ad6884c37fc251860e0d95bc62584bce2a
Dorseuil Nicolas <nicodorseuil@yahoo.fr>
2025-03-25 14:49:18 +0100

    fix rebase

--- examples/e2e/app-router/open-next.config.ts
+++ examples/e2e/app-router/open-next.config.ts
@@ -2,6 +2,7 @@ import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 import shardedTagCache from "@opennextjs/cloudflare/overrides/tag-cache/do-sharded-tag-cache";
 import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
+import queueCache from "@opennextjs/cloudflare/overrides/queue/queue-cache"
 
 export default defineCloudflareConfig({
   incrementalCache: r2IncrementalCache,
@@ -14,5 +15,5 @@ export default defineCloudflareConfig({
     },
   }),
   enableCacheInterception: true,
-  queue: doQueue({ enableRegionalCache: true, regionalCacheTtlSec: 5 }),
+  queue: queueCache(doQueue),
 });
