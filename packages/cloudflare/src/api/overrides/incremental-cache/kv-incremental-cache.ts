d77da467d57f70354f48c6c6018adff0edbe174a
James <james@eli.cx>
2025-04-07 20:16:04 +0100

    add simplified new kv inc cache

---
+++ packages/cloudflare/src/api/overrides/incremental-cache/kv-incremental-cache.ts
@@ -0,0 +1,89 @@
+import { debug, error } from "@opennextjs/aws/adapters/logger.js";
+import type { CacheValue, IncrementalCache, WithLastModified } from "@opennextjs/aws/types/overrides.js";
+import { IgnorableError } from "@opennextjs/aws/utils/error.js";
+
+import { getCloudflareContext } from "../../cloudflare-context.js";
+import { IncrementalCacheEntry } from "./internal.js";
+
+export const NAME = "cf-kv-incremental-cache";
+
+/**
+ * Open Next cache based on Cloudflare KV.
+ *
+ * Note: The class is instantiated outside of the request context.
+ * The cloudflare context and process.env are not initialized yet
+ * when the constructor is called.
+ */
+class KVIncrementalCache implements IncrementalCache {
+  readonly name = NAME;
+
+  async get<IsFetch extends boolean = false>(
+    key: string,
+    isFetch?: IsFetch
+  ): Promise<WithLastModified<CacheValue<IsFetch>> | null> {
+    const kv = getCloudflareContext().env.NEXT_INC_CACHE_KV;
+    if (!kv) throw new IgnorableError("No KV Namespace");
+
+    debug(`Get ${key}`);
+
+    try {
+      const entry = await kv.get<IncrementalCacheEntry<IsFetch>>(this.getKVKey(key, isFetch), "json");
+
+      return entry;
+    } catch (e) {
+      error("Failed to get from cache", e);
+      return null;
+    }
+  }
+
+  async set<IsFetch extends boolean = false>(
+    key: string,
+    value: CacheValue<IsFetch>,
+    isFetch?: IsFetch
+  ): Promise<void> {
+    const kv = getCloudflareContext().env.NEXT_INC_CACHE_KV;
+    if (!kv) throw new IgnorableError("No KV Namespace");
+
+    debug(`Set ${key}`);
+
+    try {
+      await kv.put(
+        this.getKVKey(key, isFetch),
+        JSON.stringify({
+          value,
+          // Note: `Date.now()` returns the time of the last IO rather than the actual time.
+          //       See https://developers.cloudflare.com/workers/reference/security-model/
+          lastModified: Date.now(),
+        }),
+        {
+          // When available, we only cache for the max revalidate time
+          ...(value.revalidate && {
+            expirationTtl: value.revalidate,
+          }),
+        }
+      );
+    } catch (e) {
+      error("Failed to set to cache", e);
+    }
+  }
+
+  async delete(key: string): Promise<void> {
+    const kv = getCloudflareContext().env.NEXT_INC_CACHE_KV;
+    if (!kv) throw new IgnorableError("No KV Namespace");
+
+    debug(`Delete ${key}`);
+
+    try {
+      await kv.delete(this.getKVKey(key, /* isFetch= */ false));
+    } catch (e) {
+      error("Failed to delete from cache", e);
+    }
+  }
+
+  protected getKVKey(key: string, isFetch?: boolean): string {
+    const buildId = process.env.NEXT_BUILD_ID ?? "no-build-id";
+    return `${buildId}/${key}.${isFetch ? "fetch" : "cache"}`.replace(/\/+/g, "/");
+  }
+}
+
+export default new KVIncrementalCache();
