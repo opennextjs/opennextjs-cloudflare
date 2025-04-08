a2410f4a5f676aa6391dbd3bb5904ede315dafeb
James <james@eli.cx>
2025-04-08 09:52:14 +0100

    update comment

--- packages/cloudflare/src/api/overrides/incremental-cache/static-assets-incremental-cache.ts
+++ packages/cloudflare/src/api/overrides/incremental-cache/static-assets-incremental-cache.ts
@@ -1,17 +1,19 @@
-import { debug, error } from "@opennextjs/aws/adapters/logger.js";
+import { error } from "@opennextjs/aws/adapters/logger.js";
 import type { CacheValue, IncrementalCache, WithLastModified } from "@opennextjs/aws/types/overrides.js";
 import { IgnorableError } from "@opennextjs/aws/utils/error.js";
 
 import { getCloudflareContext } from "../../cloudflare-context.js";
 import { debugCache, FALLBACK_BUILD_ID } from "../internal.js";
 
+//  Assets inside `cdn-cgi/...` are only accessible by the worker.
 export const CACHE_DIR = "cdn-cgi/_next_cache";
 
 export const NAME = "cf-static-assets-incremental-cache";
 
 /**
- * This cache uses Workers static assets and is not recommended. It should only be used for applications
- * that do NOT want revalidation and ONLY want to serve pre-rendered data.
+ * This cache uses Workers static assets.
+ *
+ * It should only be used for applications that do NOT want revalidation and ONLY want to serve prerendered data.
  */
 class StaticAssetsIncrementalCache implements IncrementalCache {
   readonly name = NAME;
