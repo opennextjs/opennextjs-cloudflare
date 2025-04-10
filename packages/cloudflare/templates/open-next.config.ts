cfefc2759cbfe106f23ccf3610afa08919fa4bad
Victor Berchet <victor@suumit.com>
2025-04-10 13:19:59 +0200

    Use R2 incremental cache for the template

--- packages/cloudflare/templates/open-next.config.ts
+++ packages/cloudflare/templates/open-next.config.ts
@@ -1,7 +1,7 @@
 // default open-next.config.ts file created by @opennextjs/cloudflare
 import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
-import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
+import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 
 export default defineCloudflareConfig({
-  incrementalCache: kvIncrementalCache,
+  incrementalCache: r2IncrementalCache,
 });
