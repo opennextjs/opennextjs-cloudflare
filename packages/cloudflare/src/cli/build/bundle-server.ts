cf2806cacba705cc85405d299a7d353be16ffcf3
Victor Berchet <victor@suumit.com>
2025-04-18 14:37:34 +0200

    Allow using external packages with a workerd build condition Update packages/cloudflare/src/cli/build/utils/workerd.spec.ts
    Co-authored-by: conico974 <nicodorseuil@yahoo.fr>
    fixup! feedback
    fixup! bump aws to 3.5.8

--- packages/cloudflare/src/cli/build/bundle-server.ts
+++ packages/cloudflare/src/cli/build/bundle-server.ts
@@ -77,16 +77,7 @@ export async function bundleServer(buildOpts: BuildOptions): Promise<void> {
     target: "esnext",
     minify: false,
     metafile: true,
-    // Next traces files using the default conditions from `nft` (`node`, `require`, `import` and `default`)
-    //
-    // Because we use the `node` platform for this build, the "module" condition is used when no conditions are defined.
-    // We explicitly set the conditions to an empty array to disable the "module" condition in order to match Next tracing.
-    //
-    // See:
-    // - default nft conditions: https://github.com/vercel/nft/blob/2b55b01/readme.md#exports--imports
-    // - Next no explicit override: https://github.com/vercel/next.js/blob/2efcf11/packages/next/src/build/collect-build-traces.ts#L287
-    // - ESBuild `node` platform: https://esbuild.github.io/api/#platform
-    conditions: [],
+    conditions: ["workerd"],
     plugins: [
       shimRequireHook(buildOpts),
       inlineDynamicRequires(updater, buildOpts),
@@ -158,7 +149,9 @@ export async function bundleServer(buildOpts: BuildOptions): Promise<void> {
     );
   }
 
-  console.log(`\x1b[35mWorker saved in \`${getOutputWorkerPath(buildOpts)}\` 🚀\n\x1b[0m`);
+  console.log(
+    `\x1b[35mWorker saved in \`${path.relative(buildOpts.appPath, getOutputWorkerPath(buildOpts))}\` 🚀\n\x1b[0m`
+  );
 }
 
 /**
