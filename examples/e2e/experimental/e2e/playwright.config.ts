5662d411686cd27d6d4a7cc7b256ab276a6c0bff
conico974 <nicodorseuil@yahoo.fr>
2025-05-05 11:06:25 +0200

    sync aws #849 (#633)

--- examples/e2e/experimental/e2e/playwright.config.ts
+++ examples/e2e/experimental/e2e/playwright.config.ts
@@ -1,4 +1,3 @@
 import { configurePlaywright } from "../../../common/config-e2e";
 
-// We need to disable parallel execution for the experimental app, otherwise it breaks the SSR test
-export default configurePlaywright("experimental", { parallel: false });
+export default configurePlaywright("experimental");
