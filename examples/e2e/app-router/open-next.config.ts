7810d8d70da4a06a6966e0668f776c6a72829382
Dorseuil Nicolas <nicodorseuil@yahoo.fr>
2025-05-08 14:30:58 +0200

    basic purge cache

--- examples/e2e/app-router/open-next.config.ts
+++ examples/e2e/app-router/open-next.config.ts
@@ -1,10 +1,12 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
+import {withRegionalCache} from "@opennextjs/cloudflare/overrides/incremental-cache/regional-cache";
 import shardedTagCache from "@opennextjs/cloudflare/overrides/tag-cache/do-sharded-tag-cache";
 import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
+import cachePurge from "@opennextjs/cloudflare/overrides/cache-purge/index";
 
 export default defineCloudflareConfig({
-  incrementalCache: r2IncrementalCache,
+  incrementalCache: withRegionalCache(r2IncrementalCache, {mode: "long-lived"}),
   // With such a configuration, we could have up to 12 * (8 + 2) = 120 Durable Objects instances
   tagCache: shardedTagCache({
     baseShardSize: 12,
@@ -14,5 +16,6 @@ export default defineCloudflareConfig({
     },
   }),
   queue: doQueue,
+  cachePurge: cachePurge,
   enableCacheInterception: true,
 });
