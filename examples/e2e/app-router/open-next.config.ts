76d55cf40d33b57151652597777107411c8b3847
Dorseuil Nicolas <nicodorseuil@yahoo.fr>
2025-05-12 13:43:11 +0200

    add cache-tag

--- examples/e2e/app-router/open-next.config.ts
+++ examples/e2e/app-router/open-next.config.ts
@@ -2,7 +2,7 @@ import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 import shardedTagCache from "@opennextjs/cloudflare/overrides/tag-cache/do-sharded-tag-cache";
 import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
-import queueCache from "@opennextjs/cloudflare/overrides/queue/queue-cache"
+import queueCache from "@opennextjs/cloudflare/overrides/queue/queue-cache";
 
 export default defineCloudflareConfig({
   incrementalCache: r2IncrementalCache,
