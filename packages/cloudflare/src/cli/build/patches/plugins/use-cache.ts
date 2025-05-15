feb4335fa67d5437c11eeb1eac8e0507c331d4c5
Nicolas Dorseuil <nicolas@gitbook.io>
2025-05-15 16:38:54 +0200

    fix use cache with fetch and ISR

---
+++ packages/cloudflare/src/cli/build/patches/plugins/use-cache.ts
@@ -0,0 +1,42 @@
+/**
+ * This patch will replace the createSnapshot function in the
+ * server/app-render/async-local-storage.js file to an empty string.
+ * This is necessary because the createSnapshot function is causing I/O issues for 
+ * ISR/SSG revalidation in Cloudflare Workers.
+ * TODO: Find a better fix for this issue.
+ */
+import { patchCode } from "@opennextjs/aws/build/patch/astCodePatcher.js";
+import type { CodePatcher } from "@opennextjs/aws/build/patch/codePatcher.js";
+import { getCrossPlatformPathRegex } from "@opennextjs/aws/utils/regex.js";
+
+export const rule = `
+rule:
+  kind: if_statement
+  inside:
+    kind: function_declaration
+    stopBy: end
+    has:
+      kind: identifier
+      pattern: createSnapshot
+fix:
+  '// Ignored snapshot'
+`;
+
+export const patchUseCacheIO: CodePatcher = {
+  name: "patch-use-cache",
+  patches: [
+    {
+      versions: ">=15.3.1",
+      field: {
+        pathFilter: getCrossPlatformPathRegex(
+          String.raw`server/app-render/async-local-storage\.js$`,
+          {
+            escape: false,
+          }
+        ),
+        contentFilter: /createSnapshot/,
+        patchCode: async ({ code }) => patchCode(code, rule),
+      },
+    },
+  ],
+};
