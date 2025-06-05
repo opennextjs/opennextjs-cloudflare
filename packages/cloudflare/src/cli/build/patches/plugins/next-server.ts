1ad090a5f20e2827af083562dd21300212985f59
Victor Berchet <victor@suumit.com>
2025-06-05 21:23:24 +0200

    Drop the patch for NextServer#getMiddlewareManifest

--- packages/cloudflare/src/cli/build/patches/plugins/next-server.ts
+++ packages/cloudflare/src/cli/build/patches/plugins/next-server.ts
@@ -4,11 +4,9 @@
  * Note: we will probably need to revisit the patches when the Next adapter API lands
  *
  * - Inline `getBuildId` as it relies on `readFileSync` that is not supported by workerd
- * - Inline the middleware manifest
  * - Override the cache and composable cache handlers
  */
 
-import { existsSync, readFileSync } from "node:fs";
 import path from "node:path";
 
 import { type BuildOptions, getPackagePath } from "@opennextjs/aws/build/helper.js";
@@ -30,19 +28,6 @@ export function patchNextServer(updater: ContentUpdater, buildOpts: BuildOptions
           const { outputDir } = buildOpts;
 
           contents = patchCode(contents, buildIdRule);
-
-          const manifestPath = path.join(
-            outputDir,
-            "server-functions/default",
-            getPackagePath(buildOpts),
-            ".next/server/middleware-manifest.json"
-          );
-
-          const manifest = existsSync(manifestPath)
-            ? JSON.parse(await readFileSync(manifestPath, "utf-8"))
-            : {};
-
-          contents = patchCode(contents, createMiddlewareManifestRule(manifest));
 
           const outputPath = path.join(outputDir, "server-functions/default");
           const cacheHandler = path.join(outputPath, getPackagePath(buildOpts), "cache.cjs");
@@ -71,19 +56,6 @@ fix: |-
     return process.env.NEXT_BUILD_ID;
   }
 `;
-
-export function createMiddlewareManifestRule(manifest: unknown) {
-  return `
-rule:
-  pattern:
-    selector: method_definition
-    context: "class { getMiddlewareManifest($$$PARAMS) { $$$_ } }"
-fix: |-
-  getMiddlewareManifest($$$PARAMS) {
-    return ${JSON.stringify(manifest)};
-  }
-`;
-}
 
 /**
  * The cache handler used by Next.js is normally defined in the config file as a path. At runtime,
