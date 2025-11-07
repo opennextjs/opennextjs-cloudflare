8cb7669b0b394c02798f98f3ba5e311bf44b794e
Pete Bacon Darwin <pbacondarwin@cloudflare.com>
2025-11-07 15:46:19 +0000

    Add standard Next template generated from create-next-app that will be used by create-cloudflare going forward (#973)

---
+++ create-cloudflare/next/next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+	/* config options here */
+};
+
+export default nextConfig;
+
+// Enable calling `getCloudflareContext()` in `next dev`.
+// See https://opennext.js.org/cloudflare/bindings#local-access-to-bindings.
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
