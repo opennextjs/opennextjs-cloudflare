5fe93c7fdd3c56db11ce4225036ea06f3a9702ab
Victor Berchet <victor@suumit.com>
2025-11-24 15:32:41 +0100

    Update e2e tests to Next 16 (webpack)

---
+++ .changeset/cold-rivers-bathe.md
@@ -0,0 +1,5 @@
+---
+"@opennextjs/cloudflare": patch
+---
+
+Update e2e tests to Next 16 (webpack)
--- examples/overrides/kv-tag-next/next.config.ts
+++ examples/overrides/kv-tag-next/next.config.ts
@@ -5,7 +5,6 @@ initOpenNextCloudflareForDev();
 
 const nextConfig: NextConfig = {
 	typescript: { ignoreBuildErrors: true },
-	eslint: { ignoreDuringBuilds: true },
 };
 
 export default nextConfig;
