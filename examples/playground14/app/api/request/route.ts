364b7d91b221cddf0139f6875319de605a8fde77
Victor Berchet <victor@suumit.com>
2026-05-06 21:30:12 +0200

    Bump react and next (#1255)

--- examples/playground14/app/api/request/route.ts
+++
@@ -1,5 +0,0 @@
-import { NextRequest } from "next/server";
-
-export const GET = (request: NextRequest) => {
-	return new Response(JSON.stringify({ nextUrl: request.nextUrl.href, url: request.url }));
-};
